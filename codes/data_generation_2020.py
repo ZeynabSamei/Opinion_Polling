3677da4f06613432d4bc826bc142d5e9a73cfad2
ZeynabSamei <80546485+ZeynabSamei@users.noreply.github.com>
2025-12-22 17:42:51 +0330

    Update data_generation_2020.py

--- codes/data_generation_2020.py
+++ codes/data_generation_2020.py
@@ -25,7 +25,6 @@ csv_filename   = OUTPUT_DIR / "anes_2020_chat_finetune.csv"
 
 
 
-
 # ==========================================
 # 1. Load and Clean Data
 # ==========================================
@@ -33,25 +32,23 @@ csv_filename   = OUTPUT_DIR / "anes_2020_chat_finetune.csv"
 df = pd.read_csv(file_path)
 
 COLS_MAP = {
-    "V242096x": "vote_choice", ##
-    "V241550": "gender", ##
-    "V241501x": "race", ##
-    "V241458x": "age", ##
-    "V241177": "ideology", ##
-    "V242400": "pol_interest",##
-    "V241439": "church_attendance", ##
+    "V202110x": "vote_choice",
+    "V201600": "gender",
+    "V201549x": "race",
+    "V201507x": "age",
+    "V201200": "ideology",
+    "V201231x": "party_id",
+    "V202406": "pol_interest",
+    "V201452": "church_attendance",
+    "V202022": "discuss_politics"
 }
-
 
 df = df[list(COLS_MAP.keys())].rename(columns=COLS_MAP)
 
 FEATURE_COLS = [c for c in df.columns if c != "vote_choice"]
 
 df = df[(df[FEATURE_COLS] >= 0).all(axis=1)]
-
 df = df[df["vote_choice"].isin([1, 2])].copy()
-
-
 
 print("Final dataset size:", df.shape)
 
@@ -72,12 +69,25 @@ IDEOLOGY_MAP = {
     6: "conservative", 7: "extremely conservative"
 }
 
+PARTY_MAP = {
+    1: "a strong Democrat",
+    2: "not a very strong Democrat",
+    3: "an independent Democrat",
+    4: "an independent",
+    5: "an independent Republican",
+    6: "not a very strong Republican",
+    7: "a strong Republican"
+}
 
 INTEREST_MAP = {
     1: "very", 2: "somewhat",
     3: "not very", 4: "not at all"
 }
 
+DISCUSS_MAP = {
+    1: "I like to discuss politics with my family and friends.",
+    2: "I never discuss politics with my family or friends."
+}
 
 CHURCH_MAP = {
     2: "do not attend church",
@@ -91,7 +101,7 @@ CHURCH_MAP = {
 SYSTEM_PROMPT = (
     "You are an expert political analyst specializing in US elections and voting behavior. "
     "Your task is to analyze the demographic profile provided in the text and predict the vote "
-    "choice in the 2024 Election. Output strictly one name: 'Donald Trump' or 'Kamala Harris'."
+    "choice in the 2020 Election. Output strictly one name: 'Donald Trump' or 'Joe Biden'."
 )
 
 chat_data = []
@@ -108,8 +118,10 @@ for _, row in df.iterrows():
             "gender": GENDER_MAP[row["gender"]],
             "age": int(row["age"]),
             "ideology": IDEOLOGY_MAP[row["ideology"]],
+            "party_id": PARTY_MAP[row["party_id"]],
             "church_attendance": CHURCH_MAP[row["church_attendance"]],
             "pol_interest": INTEREST_MAP[row["pol_interest"]],
+            "discuss_politics": DISCUSS_MAP[row["discuss_politics"]],
         }
 
         user_text = (
@@ -117,13 +129,15 @@ for _, row in df.iterrows():
             f"I am a {text_features['gender']}. "
             f"I am {text_features['age']} years old. "
             f"Ideologically, I am {text_features['ideology']}. "
+            f"Politically, I am {text_features['party_id']}. "
             f"I {text_features['church_attendance']}. "
             f"I am {text_features['pol_interest']} interested in politics. "
-
+            f"{text_features['discuss_politics']} "
+            "In the 2020 presidential election, I voted for "
         )
 
         assistant_text = (
-            "Kamala Harris" if row["vote_choice"] == 1 else "Donald Trump"
+            "Joe Biden" if row["vote_choice"] == 1 else "Donald Trump"
         )
 
         chat_data.append({
@@ -154,7 +168,6 @@ SEED = 42
 random.seed(SEED)
 
 combined = list(zip(chat_data, csv_rows))
-print(combined)
 random.shuffle(combined)
 chat_data, csv_rows = zip(*combined)
 
